a194584aa49a8baa07a2bcebbfa071680209942e
harry <hhallows96@gmail.com>
2020-07-14 11:33:52 +0100

    added script to rotate the airplane

---
+++ DeliveryGame/Assets/Code/Vehicle/PlaneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+
+    float horizontal;
+    float vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputControl();
+        MovementControl();
+    }
+
+    void InputControl()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+
+    }
+
+    void MovementControl()
+    {
+        transform.Rotate(vertical, 0, -horizontal);
+    }
+
+}
